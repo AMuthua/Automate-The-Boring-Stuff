24a66be4182f8fe73696f0794de6a42198c16cd5
Alexander <alexandermuthua@gmail.com>
2024-10-07 11:13:44 +0300

    The Beginning
    Hello Python :+1:

---
+++ Hello.py
@@ -0,0 +1,17 @@
+# This program says hello and asks for my name
+
+print('Hello, Welcome to automation!')
+print('What is your name?')
+
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('I hear you have a birthday soon, How old are you?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
+
+
+str(29)
+
+print('I am ' + str(29) + ' Years Old!')
---
+++ vampire.py
@@ -0,0 +1,12 @@
+print('What is your name?')
+name = input()
+print('Uko miaka ngapi wewe kichana?')
+age = str(input())
+if name == 'Zorro':
+    print('Hi, Zorro!')
+elif age < 12:
+    print('You are not Zorro, Kiddo!')
+elif age > 2000:
+    print('Unlike you, Zorro is not an undead, immortal oldie.')
+elif age > 100:
+    print('You are not Zorro, Granpa!, ahaha.')
