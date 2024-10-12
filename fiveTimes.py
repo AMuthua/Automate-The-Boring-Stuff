51a0c850a22bff74394bb60594f8a12c151d5399
Alexander <alexandermuthua@gmail.com>
2024-10-12 12:15:29 +0300

    Hmm, some more python.
    There are descriptive comments inside that explain some code, and WHY some code behaves like that.

---
+++ fiveTimes.py
@@ -0,0 +1,12 @@
+#print('My name is: ')
+#for i in range(11):
+ #   print('Boots Eleven Times (' + str(i) + ')')
+# Remember, we start from 0, all is well.
+
+
+
+print('My name is: ')
+i = 0
+while i < 11:
+    print('Boots Eleven Times ('+ str(i) + ')')
+    i = i + 1
---
+++ simpleForCalculator.py
@@ -0,0 +1,13 @@
+total = 0
+for i in range(101):
+    total = total + i
+print(total)
+
+# Consider this story about the mathematician:-
+#       Karl Friedrich Gauss.
+#   Gauss figured out that:-
+#       There were 50 pairs of numbers that added up to 100:
+#       1 + 99, 2 + 98, 3 + 97, and so on, until 49 + 51.
+#       Since 50 × 100 is 5,000
+#       When you add that middle 50
+#       The sum of all the numbers from 0 to 100 is 5,050.
